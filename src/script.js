4e97d9b24d2b2243569b47c796c535d29cebda7c
Daniel Oliveira <danmiranda227@gmail.com>
2026-09-15 12:33:24 +0100

    chore: set up the spin of the galaxy

--- src/script.js
+++ src/script.js
@@ -22,6 +22,7 @@ parameters.count = 100000;
 parameters.size = 0.01;
 parameters.radius = 5;
 parameters.branches = 3;
+parameters.spin = 1;
 
 let geometry = null;
 let material = null;
@@ -48,11 +49,12 @@ const generateGalaxy = () => {
     const i3 = i * 3;
 
     const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
     const branchesAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-    positions[i3] = Math.cos(branchesAngle) * radius;
+    positions[i3] = Math.cos(branchesAngle + spinAngle) * radius;
     positions[i3 + 1] = 0;
-    positions[i3 + 2] = Math.sin(branchesAngle) * radius;
+    positions[i3 + 2] = Math.sin(branchesAngle + spinAngle) * radius;
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 
@@ -79,6 +81,7 @@ gui.add(parameters, "count").min(100).max(1000000).step(100).onFinishChange(gene
 gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, "radius").min(0.001).max(20).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, "branches").min(2).max(20).step(1).onFinishChange(generateGalaxy);
+gui.add(parameters, "spin").min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
 
 /**
  * Sizes
