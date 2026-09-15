90dd971f66688781ade7d510f6b7862a4a2f997c
Daniel Oliveira <danmiranda227@gmail.com>
2026-09-15 11:52:40 +0100

    chore: add some tweaks to gui to set up size and count of points

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ import GUI from "lil-gui";
  * Base
  */
 // Debug
-const gui = new GUI();
+const gui = new GUI({ width: 360 });
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -18,14 +18,27 @@ const scene = new THREE.Scene();
  * Galaxy
  */
 const parameters = {};
-parameters.count = 1000;
-parameters.size = 0.02;
+parameters.count = 100000;
+parameters.size = 0.01;
+
+let geometry = null;
+let material = null;
+let points = null;
 
 const generateGalaxy = () => {
   /**
+   * Destroy old variables
+   */
+  if (points !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
+
+  /**
    * Geometry
    */
-  const geometry = new THREE.BufferGeometry();
+  geometry = new THREE.BufferGeometry();
 
   const positions = new Float32Array(parameters.count * 3);
 
@@ -40,7 +53,7 @@ const generateGalaxy = () => {
   /**
    * Material
    */
-  const material = new THREE.PointsMaterial({
+  material = new THREE.PointsMaterial({
     size: parameters.size,
     sizeAttenuation: true,
     depthWrite: false,
@@ -50,12 +63,14 @@ const generateGalaxy = () => {
   /**
    * Points
    */
-  const points = new THREE.Points(geometry, material);
-
+  points = new THREE.Points(geometry, material);
   scene.add(points);
 };
 
 generateGalaxy();
+
+gui.add(parameters, "count").min(100).max(1000000).step(100).onFinishChange(generateGalaxy);
+gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
 
 /**
  * Sizes
