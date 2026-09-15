04817021f501bc7dc075d062daf8693ff9bfae37
Daniel Oliveira <danmiranda227@gmail.com>
2026-09-15 12:48:05 +0100

    chore: set up the randomness

--- src/script.js
+++ src/script.js
@@ -23,6 +23,8 @@ parameters.size = 0.01;
 parameters.radius = 5;
 parameters.branches = 3;
 parameters.spin = 1;
+parameters.randomness = 0.2;
+parameters.randomnessPower = 3;
 
 let geometry = null;
 let material = null;
@@ -52,9 +54,16 @@ const generateGalaxy = () => {
     const spinAngle = radius * parameters.spin;
     const branchesAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
 
-    positions[i3] = Math.cos(branchesAngle + spinAngle) * radius;
-    positions[i3 + 1] = 0;
-    positions[i3 + 2] = Math.sin(branchesAngle + spinAngle) * radius;
+    const randomX =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+    const randomY =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+    const randomZ =
+      Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+
+    positions[i3] = Math.cos(branchesAngle + spinAngle) * radius + randomX;
+    positions[i3 + 1] = randomY;
+    positions[i3 + 2] = Math.sin(branchesAngle + spinAngle) * radius + randomZ;
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
 
@@ -82,6 +91,8 @@ gui.add(parameters, "size").min(0.001).max(0.1).step(0.001).onFinishChange(gener
 gui.add(parameters, "radius").min(0.001).max(20).step(0.001).onFinishChange(generateGalaxy);
 gui.add(parameters, "branches").min(2).max(20).step(1).onFinishChange(generateGalaxy);
 gui.add(parameters, "spin").min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, "randomness").min(0).max(2).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, "randomnessPower").min(1).max(10).step(0.001).onFinishChange(generateGalaxy);
 
 /**
  * Sizes
